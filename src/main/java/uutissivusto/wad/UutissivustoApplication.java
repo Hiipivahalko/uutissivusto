128ae98528ed68c43b058e11a151cbe045835688
Ossi <ossi.siipola@helsinki.fi>
2017-12-23 12:19:40 +0200

    lets bring the body

---
+++ src/main/java/uutissivusto/wad/UutissivustoApplication.java
@@ -0,0 +1,16 @@
+
+package uutissivusto.wad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class UutissivustoApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(UutissivustoApplication.class, args);
+    }
+    
+}
